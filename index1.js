5e323f176544be79bde415917d91f14d36114d76
unknown <nicolas.rose@W398.orckestra.local>
2015-06-09 14:28:37 -0400

    Add sqlite to the project

---
+++ index1.js
@@ -0,0 +1,34 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./ChinookDatabase1.4_Sqlite/Chinook_Sqlite.sqlite');
+
+db.serialize(function() {
+  //db.run("CREATE TABLE lorem (info TEXT)");
+
+  /*var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  INSERT INTO employees VALUES (1,'JOHNSON','ADMIN',6,'1990-12-17',18000,NULL,4);
+  stmt.finalize();
+*/
+
+/*
+	db.run("INSERT INTO artist ('ArtistId','Name') VALUES ($id,$name)", {
+		$id: 999,
+		$name: "Nico"
+	});
+	
+	db.each("SELECT * FROM Artist", function(err, row) {
+		console.log(row.ArtistId + ": " + row.Name);
+	});
+	*/
+	
+	db.each("SELECT * FROM sqlite_master ", function(err, row) {
+		console.log(row.type.toUpperCase() + ": " + row.name);
+		//console.log(row);
+	});
+	
+
+});
+
+db.close();
